6e885a9037078bea32db46b46739e5f09fce2325
Marshilla Brahma <marshillabrahma@zipcoders-MBP-8.fios-router.home>
2019-02-11 01:41:42 -0500

    Completed WriteIFs code

--- WriteIFs.java
+++ WriteIFs.java
@@ -7,20 +7,31 @@
  */
 public class WriteIFs
 {
- 
+    public String ss;
+    public int x;
+    public int tt_t;
+    public int tt_s;
+    public int oo1;
+    public int oo2;
+    
     public void playerDied(boolean player1) {
         // Write an IF statement that checks “player1.isAlive()” 
         // and if that’s false, calls “displayGameOver(player1)”
-     
+        boolean result = isAlive(player1);
+        if(result == false){
+            displayGameOver(player1);
+        } 
     }
     
     public String thermoSTAT(int room) {
         // Write an IF statement that checks the 
         // “temperature(room)” and if that check is less than 70, 
         // calls “heatOn()” else calls “coolOn()”
-
-
-        
+        if(tempurature(room) < 70){
+            this.heatOn();
+        }else{ 
+            this.coolOn();
+        }
         return this.ss;
     }
 
@@ -30,13 +41,18 @@ public class WriteIFs
         // AND 
         // “insideTemp()” is less than 62, 
         // calls “startAFire(fireplace1)”
+        if((outsideTemp() < 50 ) && (insideTemp() < 62)){
+           startAFire(fireplace1);
+        }
 
     }
 
     public void checkFuel(double fuelLevel) {
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
-
+        if(fuelLevel < 0.08){
+            this.refuel();
+        }
     }
 
 
